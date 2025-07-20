05ca3def0f757d4333197ea3d80752d47295c1ed
tauhid <tauhidbd789@gmail.com>
2025-07-20 08:37:48 +0600

    sidebar content  added

---
+++ src/components/SidebarContent.jsx
@@ -0,0 +1,57 @@
+import { AlertTriangle, FilePlus, Home, List, Megaphone, UserCircle, Users } from "lucide-react";
+import { Link, NavLink } from "react-router";
+
+const SidebarContent = ({ role, isLoading, linkClass }) => {
+  return (
+    <>
+      <Link to="/" className="flex items-center gap-2 mb-6 px-2">
+        <img src="/logo.png" alt="Logo" className="w-8 h-8" />
+        <span className="text-[#007dff] text-lg font-bold">DevDialogue</span>
+      </Link>
+
+      {isLoading ? (
+        <p className="text-gray-500">Loading sidebar...</p>
+      ) : (
+        <nav className="space-y-2 text-gray-800 text-sm">
+          {role === "admin" ? (
+            <>
+              <NavLink to="/dashboard/admin-profile" className={linkClass}>
+                <UserCircle size={18} /> Admin Profile
+              </NavLink>
+              <NavLink to="/dashboard/manage-users" className={linkClass}>
+                <Users size={18} /> Manage Users
+              </NavLink>
+              <NavLink
+               to="/dashboard/reported-comments" className={linkClass}>
+                <AlertTriangle size={18} /> Reported Comments
+              </NavLink>
+              <NavLink to="/dashboard/make-announcement" className={linkClass}>
+                <Megaphone size={18} /> Make Announcement
+              </NavLink>
+            </>
+          ) : (
+            <>
+              <NavLink to="/dashboard/my-profile" className={linkClass}>
+                <UserCircle size={18} /> My Profile
+              </NavLink>
+              <NavLink to="/dashboard/add-post" className={linkClass}>
+                <FilePlus size={18} /> Add Post
+              </NavLink>
+              <NavLink to="/dashboard/my-posts" className={linkClass}>
+                <List size={18} /> My Posts
+              </NavLink>
+            </>
+          )}
+
+          <div className="pt-4 border-t mt-4">
+            <NavLink to="/" className={linkClass}>
+              <Home size={18} /> Back to Home
+            </NavLink>
+          </div>
+        </nav>
+      )}
+    </>
+  );
+};
+
+export default SidebarContent;
